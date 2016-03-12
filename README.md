e33e7dfaded541f5220d061188f3df040a2214a5
evankozliner <evankozliner@gmail.com>
2016-03-12 16:12:16 -0500

    Switched to application level auth

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 Utilizes RNN's and the twitter API to generate some pretty silly tweets
 
 ## Data Gatherer
-The data gatherer is run every 15 minutes until the data cap is reached. We can only query 100 tweets per search, 
+The data gatherer is run every 15 minutes until the data cap is reached. We can only query 100 tweets per search
--- data_gatherer.py
+++ data_gatherer.py
@@ -1,9 +1,10 @@
 import json
-from twitter import Twitter, OAuth, TwitterHTTPError, TwitterStream
+from twitter import Twitter, OAuth, OAuth2,TwitterHTTPError, TwitterStream, oauth2_dance
 from os import environ
 
 """
-    
+    By Evan Kozliner & Wilfred Denton
+    Gathers tweets to train a machine learning model on.
 """
 known_tweets = []
 ids = []
@@ -12,22 +13,20 @@ tweets_csv = open("tweets.csv", "w+")
 metadata_file = open("metadata.csv", "w+")
 known_tweets = map(lambda x: int(x), id_file.read().splitlines())
 WOEID = 2383660 # Yahoo Where On Earth ID for Columbus 
-NUMBER_OF_TWEETS_TO_FETCH = 15
+NUMBER_OF_TWEETS_TO_FETCH = 20
 metadata_file.write("tweets_fetched," + str(NUMBER_OF_TWEETS_TO_FETCH) + "\n")
 metadata_file.write("woeid," + str(WOEID) + "\n")
 
-ACCESS_TOKEN = environ.get('BOT_ACCESS_TOKEN')
-ACCESS_SECRET = environ.get('BOT_ACCESS_SECRET')
 CONSUMER_KEY = environ.get('BOT_CONSUMER_KEY')
 CONSUMER_SECRET = environ.get('BOT_CONSUMER_SECRET')
-oauth = OAuth(ACCESS_TOKEN, ACCESS_SECRET, CONSUMER_KEY, CONSUMER_SECRET)
-twitter = Twitter(auth=oauth)
+BEARER_TOKEN = oauth2_dance(CONSUMER_KEY, CONSUMER_SECRET)
+
+twitter = Twitter(auth=OAuth2(bearer_token=BEARER_TOKEN))
 
 columbus_trends = twitter.trends.place(_id = WOEID)
 
 top_trend_hashtag = columbus_trends[0]['trends'][0]['name']
 metadata_file.write("hashtag_searched," + top_trend_hashtag + "\n")
-# Exclude any retweets
 top_trend_hashtag = "exclude:retweets " + top_trend_hashtag
 
 tweets = twitter.search.tweets(q=top_trend_hashtag, 
@@ -45,7 +44,7 @@ tweets = filter(lambda x: (not x['id'] in known_tweets) and (not 'retweeted_stat
 
 for tweet in tweets:
     id_file.write(str(tweet['id']) + "\n")
-    tweets_csv.write(str(tweet['text'].encode('utf-8')))
+    tweets_csv.write(str(tweet['text'].encode('utf-8')) + "\n")
     
 id_file.close()
 tweets_csv.close()
