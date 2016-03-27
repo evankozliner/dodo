eb8eeceb4d4d2d6b1a59f3da365540b6c90a083f
Wilfred Denton <dentonw3@gmail.com>
2016-03-27 16:01:14 -0400

    updated to absolute path

--- data_gatherer.py
+++ data_gatherer.py
@@ -72,10 +72,10 @@ def main():
     # tweets_csv.close()
     tweets_txt.close()
     metadata_file.close()
-    training_proc = subprocess.call(["th", "/home/ubuntu/dodo/char-rnn/train.lua", "-max_epochs", "5", "-print_every", "100"])
+    training_proc = subprocess.call(["th", "char-rnn/train.lua", "-max_epochs", "5", "-print_every", "100"], shell=True)
     model_filename = max(glob.iglob(path.join('char-rnn', 'cv', '*.t7')), key=path.getctime)
     print model_filename
-    generation_proc = subprocess.call(["th", "/home/ubuntu/dodo/char-rnn/sample.lua", "/home/ubuntu/dodo/char-rnn/cv/" + model_filename, ">>", "/home/ubuntu/dodo/sample.txt"])
+    generation_proc = subprocess.call(["th", "char-rnn/sample.lua", "char-rnn/cv/" + model_filename, ">>", "sample.txt"], shell=True)
 
 def was_cutoff_reached(tweets, cutoff_date):
     """ Tests if one of the tweets time is before the cutoff date"""
